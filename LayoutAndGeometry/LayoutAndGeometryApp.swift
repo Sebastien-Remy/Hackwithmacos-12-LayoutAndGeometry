04bc1e79c3015906e461229edc3fa1ced796f8fa
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 17:37:48 +0100

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Sebastien REMY on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
