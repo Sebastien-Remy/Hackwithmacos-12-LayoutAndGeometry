471806a0893ae6c27dd48b59ef0173591fc80544
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 18:38:53 +0100

    3d rotation #2

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -11,20 +11,20 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     
     var body: some View {
-        GeometryReader { fullView in
-        ScrollView {
-            ForEach(0..<50) { index in
-                GeometryReader { geo in
-                    Text("Row #\(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 7])
-                        .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2), axis: (x:0, y: 1, z:0))
+        ScrollView (.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX / 8), axis: (x:0, y: 1, z:0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
                 }
-                .frame(height: 40)
             }
-            
-        }
         }
     }
 }
