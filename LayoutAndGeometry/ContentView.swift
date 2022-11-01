baf8985510af35e29bd3db55476df5fbf98a615c
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 18:21:26 +0100

    absolute positioning

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -19,25 +19,11 @@ extension VerticalAlignment {
 
 struct ContentView: View {
     var body: some View {
-        HStack (alignment: .midAcoundAndName) {
-            VStack {
-                Text("@twotraws")
-                    
-                Image("paul-hudson")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                    .alignmentGuide(.midAcoundAndName) { d in d[VerticalAlignment.center]
-                    }
-            }
-            VStack {
-                Text("Full name:")
-                Text("Paul Hudson")
-                    .font(.largeTitle)
-            }
-            
-            .alignmentGuide(.midAcoundAndName) { d in d[VerticalAlignment.center] }
-        }
-        .frame(width: 400, height: 800)
+        Text("Hello, world!")
+//            .position(x: 100, y: 100)
+            .offset(x: 100, y: 100)
+            .background(.red)
+            .frame(width: 500, height: 500)
     }
 }
 
